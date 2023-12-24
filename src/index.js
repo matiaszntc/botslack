7fa35c39b001d3a983026efeaaa1cf256c438aa8
matias ch <matias.chacana@checkr.com>
2023-12-24 13:05:27 -0300

    new time

--- src/index.js
+++ src/index.js
@@ -7,7 +7,7 @@ const token = process.env.SLACK_TOKEN;
 const web = new WebClient(token);
 const conversationID = process.env.CONVERSATIONID;
 
-cron.schedule("1 11,13,15 * * 1,2,3,4,5,6,7", async () => {
+cron.schedule("7 11,13,15 * * 1,2,3,4,5,6,7", async () => {
   const result = await web.chat.postMessage({
     text: "<!here>, Hey ! please check the following server (.50 .52 .56 .100) for any agent that is running longer than it should be 😊👌🐱‍🚀",
     channel: conversationID,
